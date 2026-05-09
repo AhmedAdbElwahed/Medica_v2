22627d1c1e251776c25627cb686e6d7d92fcebb9
Ahmed Abd-Elwahed <ahmedabdelwhed68@gmail.com>
2026-05-09 17:33:51 +0300

    update the app config

--- backend/src/main/java/org/hms/medica/MedicaApplication.java
+++ backend/src/main/java/org/hms/medica/MedicaApplication.java
@@ -2,26 +2,14 @@ package org.hms.medica;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.domain.AuditorAware;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-import java.util.Optional;
-
 @SpringBootApplication
-@EnableJpaAuditing
 @EnableAsync
 public class MedicaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MedicaApplication.class, args);
-	}
-
-	@Bean
-	public AuditorAware<String> auditorProvider() {
-		// TODO: Integrate with Spring Security to return current username
-		return () -> Optional.of("system");
 	}
 
 }
